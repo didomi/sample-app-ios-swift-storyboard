618646a3c54cd6aed49a9523024e9e33b384a1e6
Automatic update <tech@didomi.io>
2024-09-04 11:25:03 +0200

    fix script used to get latest Didomi SDK

--- .github/scripts/query_last_ios_sdk_version.sh
+++ .github/scripts/query_last_ios_sdk_version.sh
@@ -4,15 +4,29 @@
 # Get iOS SDK last version (latest from cocoapods)
 #----------------------------------------------------------
 
-lastVersion=""
-for line in $(pod trunk info Didomi-XCFramework); do
-  if [[ "$line" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    lastVersion=$line
-  fi
-done
+# Get last version from pod
+pod_last_version() {
+  version=""
+  temp_file=$(mktemp)
+  pod trunk info Didomi-XCFramework > "$temp_file"
+
+  while IFS= read -r line; do
+    if [[ "$line" =~ ^[[:space:]]*-[[:space:]]*([0-9]+\.[0-9]+\.[0-9]+) ]]; then
+      current_version="${BASH_REMATCH[1]}"
+      if [[ -z "$version" || $(printf '%s\n' "$version" "$current_version" | sort -V | tail -n1) == "$current_version" ]]; then
+        version=$current_version
+      fi
+    fi
+  done < "$temp_file"
+
+  rm "$temp_file"
+  echo "$version"
+}
+
+lastVersion=$(pod_last_version)
 
 if [[ ! $lastVersion =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
-  echo "Error while getting last iOS version"
+  echo "Error while getting last iOS version ($lastVersion)"
   exit 1
 fi
 
